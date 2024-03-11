a70c62c82d6a466ce955c3a4b52b9bdad10cf9f2
Эвелина Ким <elya@MacBook-Pro-Evelina.local>
2024-03-11 11:12:51 +0300

    feat: создать структуру

---
+++ src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import appStyles from './app.module.css';
+
+function App() {
+  return (
+    <div className={appStyles.App}>
+
+    </div>
+  );
+}
+
+export default App;
